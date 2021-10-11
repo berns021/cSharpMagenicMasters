1a36400464d3a6f80c60d97c11b25fcbbc998a33
Berns Caay <Berns.Caay@ey.com>
2021-10-11 19:43:16 +0800

    Assignement number 1

---
+++ CsharpsMasterAssigments/MyYeild_10_11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CsharpsMasterAssigments
+{
+    public class MyYeild_10_11
+    {
+        /// <summary>
+        /// When you use the yield contextual keyword in a statement, 
+        /// ou indicate that the method, operator, or get accessor in 
+        /// which it appears is an iterator. Using yield to define an 
+        /// iterator removes the need for an explicit extra class 
+        /// (the class that holds the state for an enumeration, see IEnumerator<T> for an example) 
+        /// when you implement the IEnumerable and IEnumerator pattern for a custom collection type.
+        /// </summary>
+        static void Main()
+        {
+            // Display powers of 2 up to the exponent of 8:
+            foreach (int i in Power(2, 8))
+            {
+                // this will prnt the return value from the yield statement 
+                Console.Write("{0} ", i);
+            }
+        }
+
+        public static System.Collections.Generic.IEnumerable<int> Power(int number, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 0; i < exponent; i++)
+            {
+                result = result * number;
+                // The use of  a yield return statement to return each element one at a time
+                yield return result;
+            }
+        }
+
+        // Output: 2 4 8 16 32 64 128 256
+    }
+
+}
